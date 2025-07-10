646a03eeb78c581f871b2a94274f60bbb78c51e7
Sam Wood <srwood07@gmail.com>
2025-07-10 11:34:20 +0100

    T177675 Front-end: updated tab styles applied.

--- BOI.Web/assets/src/scripts/pages/criteriaLookup.js
+++ BOI.Web/assets/src/scripts/pages/criteriaLookup.js
@@ -177,7 +177,7 @@ $('body').on('click', '.alphabet-list a', function (e) {
     history.replaceState(null, null, url);
 
     $('html,body').animate({
-        scrollTop: $criteriaCategory.offset().top - 120
+        scrollTop: $criteriaCategory.offset().top - $('header').outerHeight(),
     }, 500, function () {
         //after animation set the focus
         var $categoryHeading = $criteriaCategory.find('h3');
