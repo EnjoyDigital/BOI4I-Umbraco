8912064de7faeab03382012e93f3722a4e2e6175
Lewis Farrow <lewis.farrow@live.co.uk>
2025-09-02 18:19:40 +0100

    T179620 T179621 T179622 Try to tackle video issues with Plyr

--- BOI.Web/assets/src/scripts/modules/videoBlock.js
+++ BOI.Web/assets/src/scripts/modules/videoBlock.js
@@ -1,12 +1,29 @@
+import Plyr from 'plyr/dist/plyr.js';
+
 export default function VideoBlock() {
   init()
 }
 
 function init() {
-  var $playIcon = $('.play-icon')
+  $('.video-block').each(function() {
+    var $playIcon = $(this).find('.play-icon'),
+        $overlay = $(this).find('.video-block-video__overlay'),
+        $video = $(this).find('video');
 
-  $playIcon.on('click', function() {
-    $(this).parent().fadeOut(250)
-    $(this).parent().siblings('video').get(0).play()
-  })
+    var player = new Plyr($video[0], {
+      controls: ['play', 'progress', 'current-time', 'mute', 'volume', 'fullscreen'],
+      hideControls: false,
+      keyboard: { focused: true, global: false },
+      iconUrl: '/assets/dist/images/icons/plyr/plyr.svg'
+    });
+
+    $playIcon.on('click', function() {
+      player.play();
+      player.media.focus();
+    });
+
+    player.on('play', function() {
+      $overlay.fadeOut(250);
+    });
+  });  
 }
